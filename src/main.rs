cf62e2fcb8e2a3a166d406287e804f59dafb101d
Marat Bukharov <marat.buharov@gmail.com>
2024-09-06 01:47:31 +0300

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use egui::Visuals;
-use serde::{Deserialize, Serialize};
+use marshrutka::MarshrutkaApp;
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
@@ -61,93 +60,5 @@ fn main() {
                 }
             }
         }
-    });
-}
-
-#[derive(Deserialize, Serialize, Eq, PartialEq, Default)]
-#[serde(default)]
-pub struct MarshrutkaApp {}
-
-impl MarshrutkaApp {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        let result: MarshrutkaApp = if let Some(storage) = cc.storage {
-            eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
-        } else {
-            Default::default()
-        };
-        cc.egui_ctx.set_visuals(Visuals::light());
-        result
-    }
-
-    fn top_menu(&mut self, ctx: &egui::Context) {
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            egui::menu::bar(ui, |ui| {
-                egui::widgets::global_dark_light_mode_switch(ui);
-                ui.separator();
-
-                let is_web = cfg!(target_arch = "wasm32");
-                ui.menu_button("File", |ui| {
-                    // NOTE: no File->Quit on web pages
-                    if !is_web {
-                        ui.separator();
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                            ui.close_menu();
-                        }
-                    }
-                });
-            });
-        });
-    }
-}
-
-impl eframe::App for MarshrutkaApp {
-    /// Called each time the UI needs repainting, which may be many times per second.
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        self.top_menu(ctx);
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("Marshrutka");
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                bottom_frame(ui);
-                ui.separator();
-            });
-        });
-    }
-
-    /// Called by the framework to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
-    }
-}
-
-fn bottom_frame(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.horizontal(|ui| {
-            ui.spacing_mut().item_spacing.x = 0.0;
-            ui.label("Powered by ");
-            ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-            ui.label(" and ");
-            ui.hyperlink_to(
-                "eframe",
-                "https://github.com/emilk/egui/tree/master/crates/eframe",
-            );
-            ui.label(".");
-        });
-        if cfg!(debug_assertions) {
-            ui.separator();
-            egui::warn_if_debug_build(ui);
-        }
-        ui.separator();
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.hyperlink_to("Source code", "https://github.com/maratik123/marshrutka");
-        ui.label(".");
     });
 }
