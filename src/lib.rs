554088a05b1246f779d01e78b7df900769502c39
Marat Bukharov <marat.buharov@gmail.com>
2024-09-06 05:24:45 +0300

    map

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
-use egui::Visuals;
+use egui::load::{BytesLoadResult, BytesPoll};
+use egui::{ScrollArea, Visuals};
 use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
 
-#[derive(Deserialize, Serialize, Eq, PartialEq, Default)]
+#[derive(Deserialize, Serialize, Default)]
 #[serde(default)]
-pub struct MarshrutkaApp {}
+pub struct MarshrutkaApp {
+    #[serde(skip)]
+    raw_map: Option<BytesLoadResult>,
+}
 
 impl MarshrutkaApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -12,12 +17,19 @@ impl MarshrutkaApp {
 
         // Load previous app state (if any).
         // Note that you must enable the `persistence` feature for this to work.
-        let result: MarshrutkaApp = if let Some(storage) = cc.storage {
+        let mut result: MarshrutkaApp = if let Some(storage) = cc.storage {
             eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
         } else {
             Default::default()
         };
+        egui_extras::install_image_loaders(&cc.egui_ctx);
         cc.egui_ctx.set_visuals(Visuals::light());
+
+        result.raw_map = Some(
+            cc.egui_ctx
+                .try_load_bytes("https://api.chatwars.me/webview/map"),
+        );
+
         result
     }
 
@@ -48,13 +60,30 @@ impl eframe::App for MarshrutkaApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         self.top_menu(ctx);
 
+        egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                bottom_frame(ui);
+            });
+        });
+
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
             ui.heading("Marshrutka");
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                bottom_frame(ui);
-                ui.separator();
+            let s = match self.raw_map.as_ref().unwrap() {
+                Ok(BytesPoll::Pending { .. }) => {
+                    ctx.request_repaint();
+                    Cow::Borrowed(include_str!(concat!(
+                        env!("CARGO_MANIFEST_DIR"),
+                        "/Map.html"
+                    )))
+                }
+                Ok(BytesPoll::Ready { bytes, .. }) => String::from_utf8_lossy(bytes),
+                Err(e) => Cow::Owned(e.to_string()),
+            };
+
+            ScrollArea::vertical().show(ui, |ui| {
+                ui.label(s);
             });
         });
     }
