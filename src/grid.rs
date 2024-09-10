e80b56b2adc2c5df411837f21fdd0aca90ae4ef0
Marat Bukharov <maratik@yandex-team.ru>
2024-09-10 23:50:53 +0300

    tmp

--- src/grid.rs
+++ src/grid.rs
@@ -1,9 +1,15 @@
 use crate::cell::Cell;
 use anyhow::{anyhow, Result};
+use egui::ecolor::ParseHexColorError;
+use egui::Color32;
 use ndarray::{Array, Array2};
+use num_integer::Roots;
+use simplecss::DeclarationTokenizer;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
 use tl::ParserOptions;
 
-pub const CELL_SIZE: f32 = 89.0;
+pub const CELL_SIZE: f32 = 88.0;
 
 #[derive(Default)]
 pub struct MapGrid {
@@ -21,12 +27,48 @@ impl MapGrid {
             .ok_or_else(|| anyhow!("No map-grid elements found"))?;
         let map_grid = map_grid
             .get(parser)
-            .and_then(|n| n.as_tag())
+            .and_then(|n| n.as_tag().filter(|tag| tag.name() == "div"))
             .ok_or_else(|| anyhow!("Can not parse map-grid element"))?;
-        map_grid.attributes()
-        let cell_nodes = map_grid
+        let color = map_grid
+            .attributes()
+            .get("style")
+            .flatten()
+            .map(|style| {
+                let style = style.as_utf8_str();
+                DeclarationTokenizer::from(style.as_ref())
+                    .find(|v| v.name == "background-color")
+                    .map(|v| Color32::from_hex(v.value).map_err(|e| ParseHexColorErrorWrapper(e)))
+            })
+            .flatten()
+            .transpose()?;
+        let map_cells: Vec<_> = map_grid
             .children()
-            .ok_or_else(|| anyhow!("map-grid element is not tag"))?
-            .all(parser);
+            .top()
+            .iter()
+            .flat_map(|c| c.get(parser).and_then(|n| n.as_tag()))
+            .collect();
+        let square_size = map_cells.len().sqrt();
+        if square_size * square_size != map_cells.len() {
+            return Err(anyhow!(""));
+        }
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct ParseHexColorErrorWrapper(ParseHexColorError);
+
+impl Error for ParseHexColorErrorWrapper {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        if let ParseHexColorError::InvalidInt(e) = &self.0 {
+            Some(e)
+        } else {
+            None
+        }
+    }
+}
+
+impl Display for ParseHexColorErrorWrapper {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
     }
 }
