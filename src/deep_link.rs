bb1c0b16cebd2f4e765ec2b7eb8eaa8fe48c69c2
Marat Bukharov <marat.buharov@gmail.com>
2024-10-28 05:57:10 +0300

    fixes

--- src/deep_link.rs
+++ src/deep_link.rs
@@ -1,35 +1,38 @@
-use std::fmt::Display;
+macro_rules! deep_link {
+    ($mod_name:ident, $link_to_support_chat:literal, $send_command_to_bot:literal, $send_command:literal) => {
+        mod $mod_name {
+            use std::fmt::Display;
 
-#[cfg(not(target_arch = "wasm32"))]
-mod native {
-    use super::*;
+            pub const LINK_TO_SUPPORT_CHAT: &str = $link_to_support_chat;
 
-    pub const LINK_TO_SUPPORT_CHAT: &str = "https://t.me/marshrutka_support";
+            pub fn send_command_to_bot(command: impl Display) -> String {
+                format!($send_command_to_bot, command)
+            }
 
-    pub fn send_command_to_bot(command: impl Display) -> String {
-        format!("https://t.me/ChatWarsBot?text={command}")
-    }
-
-    pub fn send_command(command: impl Display) -> String {
-        format!("https://t.me/share?url={command}")
-    }
-}
-#[cfg(target_arch = "wasm32")]
-mod wasm {
-    use super::*;
-
-    pub const LINK_TO_SUPPORT_CHAT: &str = "tg://resolve?domain=marshrutka_support";
-
-    pub fn send_command_to_bot(command: impl Display) -> String {
-        format!("tg://resolve?domain=ChatWarsBot&text={command}")
-    }
-
-    pub fn send_command(command: impl Display) -> String {
-        format!("tg://msg_url?url={command}")
-    }
+            pub fn send_command(command: impl Display) -> String {
+                format!($send_command, command)
+            }
+        }
+    };
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-pub(crate) use native::*;
+deep_link!(
+    native,
+    "https://t.me/marshrutka_support",
+    "https://t.me/ChatWarsBot?text={}",
+    "https://t.me/share?url={}"
+);
+
 #[cfg(target_arch = "wasm32")]
-pub(crate) use wasm::*;
+deep_link!(
+    wasm,
+    "tg://resolve?domain=marshrutka_support",
+    "tg://resolve?domain=ChatWarsBot&text={}",
+    "tg://msg_url?url={}"
+);
+
+#[cfg(not(target_arch = "wasm32"))]
+pub use native::*;
+#[cfg(target_arch = "wasm32")]
+pub use wasm::*;
